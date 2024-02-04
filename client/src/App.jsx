7d94906291844d03c3539d3ea0f1de297818d870
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-04 16:22:31 +0200

    baisc structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <h1>Blog</h1>;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import About from "./pages/About";
+import { Signup } from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,5 @@
+const Signin = () => {
+  return <div>Sign In</div>;
+};
+
+export default Signin;
