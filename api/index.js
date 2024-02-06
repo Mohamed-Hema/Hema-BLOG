7f0f013d14450ab2f98d102d6a975bc044db75d2
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-06 18:05:37 +0200

    add middleware

--- api/index.js
+++ api/index.js
@@ -15,10 +15,21 @@ mongoose
   });
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
