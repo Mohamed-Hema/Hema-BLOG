f45130f81879d8560c29c953c1e6cea8f033411b
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-06 17:49:54 +0200

    baisc auth

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,7 +15,10 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
