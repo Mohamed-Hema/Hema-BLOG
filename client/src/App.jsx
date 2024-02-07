1e6be2e3a3dffc6f2920830a0be6a8d01c9485f2
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-07 20:51:45 +0200

    sign in functionality is done ✅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import About from "./pages/About";
 import { Signup } from "./pages/Signup";
-import Signin from "./pages/Signin";
+import { SignIn } from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
@@ -14,7 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
