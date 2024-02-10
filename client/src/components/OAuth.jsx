97c38960f71879b2bc9ac6c2852e95e5999021e0
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-10 18:57:56 +0200

    google OAUTH done

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -27,6 +27,7 @@ export default function OAuth() {
       const data = await res.json();
       if (res.ok) {
         dispatch(signInSuccess(data));
+        console.log(data);
         navigate("/");
       }
     } catch (error) {
