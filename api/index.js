4e7f9955e637250424f88f322994ac33b94949d5
Mohamed Hema <mohamedhema2017@gmail.com>
2024-03-06 20:37:37 +0200

    create post API ✅

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 import cookieParser from "cookie-parser";
+import postRoutes from "./routes/post.route.js";
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode || 500;
