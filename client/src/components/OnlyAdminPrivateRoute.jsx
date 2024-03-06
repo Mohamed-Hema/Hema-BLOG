d0152dbdaef6c4288b56c09af812f94c09a4f583
Mohamed Hema <mohamedhema2017@gmail.com>
2024-03-06 18:55:40 +0200

    create post ui ✅

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -1,6 +1,11 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
-export default function PrivateRoute() {
+
+export default function OnlyAdminPrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+  return currentUser && currentUser.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/sign-in" />
+  );
 }
