fd47eeec2431735097040f19db5e869867d27465
Mohamed Hema <mohamedhema2017@gmail.com>
2024-03-08 14:54:05 +0200

    Dashposts is done ✅ + updating brand color

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -17,8 +17,8 @@ export default function FooterCom() {
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
-              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Hema's
+              <span className="px-2 py-1 bg-gradient-to-r from-blue-900 via-blue-500 to-blue-300 rounded-lg text-white">
+                Hema&apos;s
               </span>
               Blog
             </Link>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+// Import the Flowbite plugin
 import flowbitePlugin from "flowbite/plugin";
+
+// Import the tailwind-scrollbar plugin
+import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
-
   theme: {
     extend: {},
   },
-  plugins: [flowbitePlugin],
+  plugins: [flowbitePlugin, tailwindScrollbar],
 };
