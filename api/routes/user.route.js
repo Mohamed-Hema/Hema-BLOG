97129487ebab5c4c0858e875c771e1f3d4ef4e30
Mohamed Hema <mohamedhema2017@gmail.com>
2024-03-13 22:52:15 +0200

    updates & optimization for user controller and routes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,14 +4,12 @@ import {
   getUser,
   getUsers,
   signout,
-  test,
   updateUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
